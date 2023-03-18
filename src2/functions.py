1db25785151da1bfd59d9bab35f8c6e733fa1db8
markitos002 <markitos@DESKTOP-4JITBCH.localdomain>
2023-03-18 00:06:29 -0500

    working on pyhton CS

--- src2/functions.py
+++ src2/functions.py
@@ -1,5 +1,19 @@
-def square(x):
-    return x * x
+import squares # import all functions from squares.py
 
-for i in range(10):
-    print("{} squared is {}".format(i, square(i)))
+for i in range(100):
+    print(f" The squared of {i} is {squares.square(i)}")
+
+
+"""
+from squares import square  # import the square function from squares.py
+
+#def square(x):
+#    return x * xs
+
+for i in range(100):
+   print(f" The squared of {i} is {square(i)}") # f-string another way to format strings
+
+for i in range(100):
+   print("{} squared is {}".format(i, square(i)))
+"""
+
