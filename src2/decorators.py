d0d1375818970cb6b048fec535d052d347298cbf
Marco Fidel Avila <markos.avila@gmail.com>
2023-03-23 23:53:27 -0500

    trabajo en clases, decorators y lambda

--- src2/decorators.py
+++ src2/decorators.py
@@ -1,3 +1,21 @@
+import time
+
+def measure_time(func):
+    def wrapper():
+        start = time.time()
+        func()
+        end = time.time()
+        print(f"La función {func.__name__} tardó {end-start} segundos en ejecutarse.")
+    return wrapper
+
+@measure_time
+def my_function():
+    print("Hello, world!")
+    time.sleep(1)   
+
+my_function()
+
+"""
 def announce(f):
     def wrapper():
         print("About to run the function...")
@@ -11,3 +29,5 @@ def hello():
     print("Hello, world!")
 
 hello()
+
+"""
--- src2/lambda.py
+++ src2/lambda.py
@@ -1,3 +1,20 @@
+people = [
+    {"name": "Harry", "house": "Gryffindor"},
+    {"name": "Cho", "house": "Ravenclaw"},
+    {"name": "Draco", "house": "Slytherin"},
+    {"name": "Ginny", "house": "Gryffindor"},
+    {"name": "Luna", "house": "Ravenclaw"}
+]
+print(people)
+print()
+
+people.sort(key=lambda person: person["name"])
+
+for person in people:
+    print("{name} is in {house} house".format(**person))
+
+print()
+
 movies = [
     {"title": "Green Book", "year": 2018},
     {"title": "The Shape of Water", "year": 2017},
@@ -6,7 +23,8 @@ movies = [
     {"title": "Birdman", "year": 2014},
     {"title": "12 Years a Slave", "year": 2013}
 ]
-
+print(movies)
+print()
 movies.sort(key=lambda movie: movie["year"])
 
 for movie in movies:
