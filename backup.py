f27786da4f75a52978c56f9933d1560010894ef9
Marco Fidel Avila <markos.avila@gmail.com>
2024-05-03 21:57:13 -0500

    funciones adicionales

---
+++ backup.py
@@ -0,0 +1,5 @@
+import sys
+
+print(sys.argv)
+print(sys.argv[0]) #nombre del programa
+print(sys.argv[1]) #primer argumento
---
+++ input.py
@@ -0,0 +1,6 @@
+print("Come to calculate the parsec value from light year value.")
+parsec = int(input("Enter the parsec value: "))
+light_year = parsec * 3.262
+
+print(str(parsec) + " parsec is equal to " + str(light_year) + " light years.")
+
