1890c32de15c7d4dd7c53bff58ded782f234d859
Marco Fidel Avila <markos.avila@gmail.com>
2024-05-03 20:41:37 -0500

    modified date.py

--- date.py
+++ date.py
@@ -5,3 +5,6 @@ print(date.today().year)
 print("Today is", date.today().strftime("%a %b %d %Y"))
 print("Today's date is:" + str(date.today()))
 
+parsec = 11
+light_year = 3.262 * parsec
+print(str(parsec) + " parsecs is equal to " + str(light_year) + " light years")
