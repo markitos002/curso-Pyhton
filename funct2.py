d4862caed7a84fb85b1d7e3cb49da81237652dcc
Marco Fidel Avila <markos.avila@gmail.com>
2024-06-09 23:18:22 -0500

    trabajo con funcione que piden argumentos y devuelven valores

--- funct2.py
+++ funct2.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 def generate_report(main_tank, external_tank, hydrogen_tank):
     output = f"""Fuel Report:
     Main Tank: {main_tank}
@@ -7,3 +9,19 @@ def generate_report(main_tank, external_tank, hydrogen_tank):
     print(output)
 
 generate_report(100, 200, 300)
+
+
+#tiempo de llegada de la mision apollo que tomo 51 horas
+def arrival_time(hours=51):
+    now = datetime.now()
+    arrival = now + timedelta(hours=hours)
+    return arrival.strftime("Arrival: %A %H:%M")
+
+print(arrival_time())
+
+def arrival_time(destination, hours=51):
+    now = datetime.now()
+    arrival = now + timedelta(hours=hours)
+    return arrival.strftime(f"{destination} Arrival: %A %H:%M")
+
+print(arrival_time("Orbit", hours=0.13))
